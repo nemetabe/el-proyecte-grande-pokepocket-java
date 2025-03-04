3da27aeeec74f3b30bf9c28b76358f462b87d50f
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-03-04 14:01:39 +0100

    feat/transaction/dao/getSum/getAvrg

--- Backend/src/main/java/com/codecool/backend/dao/TransactionDao.java
+++ Backend/src/main/java/com/codecool/backend/dao/TransactionDao.java
@@ -4,6 +4,12 @@ import com.codecool.backend.controller.dto.NewTransactionDto;
 
 public interface TransactionDao {
     int addTransaction(NewTransactionDto newTransactionDto);
+
     boolean updateTransaction(int id);
+
     boolean deleteTransaction(int id);
+
+    int getSumOfTransactionByCategoryId(int categoryId);
+
+    int getAvrgSpendingByCategoryId(int categoryId);
 }
