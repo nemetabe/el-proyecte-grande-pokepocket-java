b72b41d545f19487f7681b5b1cfcfd6c618bc251
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-03-04 13:58:33 +0100

    feat/transaction/service-methodes

--- Backend/src/main/java/com/codecool/backend/service/TransactionService.java
+++ Backend/src/main/java/com/codecool/backend/service/TransactionService.java
@@ -1,4 +1,36 @@
 package com.codecool.backend.service;
 
+import com.codecool.backend.controller.dto.NewTransactionDto;
+import com.codecool.backend.dao.TransactionDao;
+
 public class TransactionService {
+  private final TransactionDao transactionDao;
+
+  public TransactionService(TransactionDao transactionDao) {
+    this.transactionDao = transactionDao;
+  }
+
+  public int createTransaction(NewTransactionDto newTransactionDto) {
+      return transactionDao.addTransaction(newTransactionDto);
+  }
+
+  public boolean updateTransaction(int id) {
+      return transactionDao.updateTransaction(id);
+  }
+
+  public boolean deleteTransaction(int id) {
+      return transactionDao.deleteTransaction(id);
+  }
+
+  public int getSumOfTransactionByCategoryId(int categoryId) {
+
+  }
+
+  public int getAvrgSpendingByCategoryId(int categoryId) {
+
+  }
+
+
+
+
 }
