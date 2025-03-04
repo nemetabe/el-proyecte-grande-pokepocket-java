c0dae09f9943cb0b53409f3ef7c70ab9c29a45bd
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-03-04 13:42:58 +0100

    transaction dao and service

---
+++ Backend/src/main/java/com/codecool/backend/dao/TransactionDao.java
@@ -0,0 +1,9 @@
+package com.codecool.backend.dao;
+
+import com.codecool.backend.controller.dto.NewTransactionDto;
+
+public interface TransactionDao {
+    int addTransaction(NewTransactionDto newTransactionDto);
+    boolean updateTransaction(int id);
+    boolean deleteTransaction(int id);
+}
