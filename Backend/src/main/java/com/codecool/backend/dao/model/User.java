d7c81d400c26f762587b154cc3000c717db1b5bf
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-03-04 15:14:54 +0100

    Refactor UserDao

--- Backend/src/main/java/com/codecool/backend/dao/model/User.java
+++ Backend/src/main/java/com/codecool/backend/dao/model/User.java
@@ -36,9 +36,13 @@ public class User {
 
     public String getEmail() {
         return email;
-      
+    }
       
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
