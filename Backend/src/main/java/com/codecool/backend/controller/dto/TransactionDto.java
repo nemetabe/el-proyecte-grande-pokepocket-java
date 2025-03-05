86dfab30cb78c3a979ad404cc337dcc817a1219a
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-03-05 11:32:10 +0100

    refact/transaction -> class, dto, memory

--- Backend/src/main/java/com/codecool/backend/controller/dto/TransactionDto.java
+++ Backend/src/main/java/com/codecool/backend/controller/dto/TransactionDto.java
@@ -4,6 +4,6 @@ import com.codecool.backend.dao.model.Transaction;
 
 public record TransactionDto(int id, String name, int category, int amount) {
     public TransactionDto(Transaction transaction){
-        this(transaction.id(), transaction.name(), transaction.categoryId(), transaction.amount());
+        this(transaction.getId(), transaction.getName(), transaction.getCategoryId(), transaction.getAmount());
     }
 }
