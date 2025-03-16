46ad81c79dc29caddf585273685bdf927d7e64d8
nemetabe <153465162+nemetabe@users.noreply.github.com>
2025-03-16 19:04:00 +0100

    Adds poke controller advice

---
+++ Backend/src/main/java/com/codecool/backend/controller/PokeControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.codecool.backend.controller;
+
+import com.codecool.backend.controller.exception.MemberNotFoundException;
+
+import com.codecool.backend.controller.exception.TransactionNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestControllerAdvice
+public class PokeControllerAdvice {
+    @ResponseBody
+    @ExceptionHandler(MemberNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String memberNotFoundExceptionHandler(MemberNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(TransactionNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String transactionNotFoundExceptionHandler(TransactionNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+
+}
