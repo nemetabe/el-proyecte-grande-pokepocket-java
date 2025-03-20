249137cf6641e0f2400be8761676a3abc57851d1
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-03-20 15:17:03 +0100

    Add db filler data sql files

--- Backend/src/main/java/com/codecool/backend/controller/dto/NewTransactionDto.java
+++ Backend/src/main/java/com/codecool/backend/controller/dto/NewTransactionDto.java
@@ -2,5 +2,5 @@ package com.codecool.backend.controller.dto;
 
 import com.codecool.backend.model.Member;
 
-public record NewTransactionDto(String name, int categoryId, int amount, int memberId) {
+public record NewTransactionDto(String name, Long categoryId, int amount, int memberId) {
 }
