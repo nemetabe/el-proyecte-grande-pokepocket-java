e5f0a7d533fc6822b6d3e1c05a527641e9142435
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-03-20 09:45:41 +0100

    Implement Pokemon controller class

---
+++ Backend/src/main/java/com/codecool/backend/controller/dto/UserPokemonDto.java
@@ -0,0 +1,21 @@
+package com.codecool.backend.controller.dto;
+
+import com.codecool.backend.model.pokemon.PokemonSpecies;
+import com.codecool.backend.model.pokemon.UserPokemon;
+
+import java.time.LocalDateTime;
+
+public record UserPokemonDto(Long pokemonId, Long speciesId, String nickName, Integer happiness, String pictureUrl, String gifUrl, LocalDateTime hatchDate, Boolean isEvolutionPending, PokemonSpecies species) {
+
+    public UserPokemonDto(UserPokemon userPokemon) {
+        this(userPokemon.getPokemonId(),
+                userPokemon.getSpeciesId(),
+                userPokemon.getNickname(),
+                userPokemon.getHappiness(),
+                userPokemon.getPictureUrl(),
+                userPokemon.getGifUrl(),
+                userPokemon.getHatchDate(),
+                userPokemon.getIsEvolutionPending(),
+                userPokemon.getSpecies());
+    }
+}
