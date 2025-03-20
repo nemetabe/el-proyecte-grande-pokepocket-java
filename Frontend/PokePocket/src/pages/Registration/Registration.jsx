065eff77cea1568201624c6a58a238bfa130c91e
TothKristof <56380808+TothKristof@users.noreply.github.com>
2025-03-20 17:20:35 +0100

    Redesign resize an div

--- Frontend/PokePocket/src/pages/Registration/Registration.jsx
+++ Frontend/PokePocket/src/pages/Registration/Registration.jsx
@@ -103,9 +103,9 @@ function Registration() {
   return (
     <>
     <div className="flex justify-center items-center w-full h-screen">
-      <div className="bg-white m-auto whitebox flex justify-between h-[600px] basis-6/12">
+      <div className="bg-white m-auto whitebox flex justify-between h-[600px] basis-8/12">
         <div
-          className={`basis-7/12 my-auto text-dark form-container ${isFadingOut ? "fade-out" : "fade-in"}`}>
+          className={`basis-8/12 my-auto text-dark form-container ${isFadingOut ? "fade-out" : "fade-in"}`}>
           {isRegistering ? (
             <>
               <RegistrationForm
