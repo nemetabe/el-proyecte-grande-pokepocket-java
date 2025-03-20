d098785051d56698592113f62811c07758437960
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-03-20 14:29:48 +0100

    Bugfix TransactionDto

--- Backend/src/main/java/com/codecool/backend/controller/dto/TransactionDto.java
+++ Backend/src/main/java/com/codecool/backend/controller/dto/TransactionDto.java
@@ -1,11 +1,12 @@
 package com.codecool.backend.controller.dto;
 
+import com.codecool.backend.model.Category;
 import com.codecool.backend.model.Member;
 import com.codecool.backend.model.Transaction;
 
-public record TransactionDto(Long id, String name, int category, int amount, Member member) {
+public record TransactionDto(Long id, String name, Category category, int amount, Member member) {
     public TransactionDto(Transaction transaction){
-        this(transaction.getId(), transaction.getName(), transaction.getId(), transaction.getAmount());
+        this(transaction.getId(), transaction.getName(), transaction.getCategory(), transaction.getAmount(), transaction.getMember());
 
     }
 }
