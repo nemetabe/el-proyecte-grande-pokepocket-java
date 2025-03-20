b2db82b8d65c995061230936ca6cd73d4c5ad5f6
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-03-20 14:40:12 +0100

    Add DataInitializer to populate the Category table, fix TransactionDto with date, fix Servise

--- Backend/src/main/java/com/codecool/backend/controller/dto/TransactionDto.java
+++ Backend/src/main/java/com/codecool/backend/controller/dto/TransactionDto.java
@@ -4,9 +4,11 @@ import com.codecool.backend.model.Category;
 import com.codecool.backend.model.Member;
 import com.codecool.backend.model.Transaction;
 
-public record TransactionDto(Long id, String name, Category category, int amount, Member member) {
+import java.time.LocalDate;
+
+public record TransactionDto(Long id, String name, Category category, int amount, Member member, LocalDate date) {
     public TransactionDto(Transaction transaction){
-        this(transaction.getId(), transaction.getName(), transaction.getCategory(), transaction.getAmount(), transaction.getMember());
+        this(transaction.getId(), transaction.getName(), transaction.getCategory(), transaction.getAmount(), transaction.getMember(), transaction.getDate());
 
     }
 }
