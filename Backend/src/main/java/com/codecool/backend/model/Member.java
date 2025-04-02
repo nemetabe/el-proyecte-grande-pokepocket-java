82a147aafb16e3718dac138981132f829ed87e5d
Orsolya Szabo <szabo.orsolya.levle@gmail.com>
2025-04-02 19:10:08 +0200

    Create new DTO for edit profile, create new endpoints, handle upodates in backends

--- Backend/src/main/java/com/codecool/backend/model/Member.java
+++ Backend/src/main/java/com/codecool/backend/model/Member.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -22,10 +23,14 @@ public class Member {
     private String name;
     private String email;
     private String password;
+
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    @Column(nullable = false)
+    private BigDecimal targetAmount;
 
     @OneToMany(mappedBy = "member")
     private List<Transaction> transactions;
